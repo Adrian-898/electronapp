d8e799c72c894bd16efc06634b954ec62d546469
Adrian-898 <adriantorres898@gmail.com>
2024-11-18 18:52:58 -0300

    content security policies added

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import electron from "electron";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +14,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count - 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
