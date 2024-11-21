95a7da14040c0c1f7ded5c39dce380594ea96e3f
Adrian-898 <adriantorres898@gmail.com>
2024-11-21 18:54:13 -0300

    añadido home

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,83 +4,101 @@ import QRScanner from "./screens/QRScanner";
 
 const App = () => {
   return (
-    <div id="app">
-      <div className="tab-content m-4">
-        <div
-          className="tab-pane"
-          id="home"
-          role="tabpanel"
-          aria-labelledby="home-tab"
-        >
-          {<Home />}
-        </div>
-        <div
-          className="tab-pane"
-          id="scanner"
-          role="tabpanel"
-          aria-labelledby="scanner-tab"
-        >
-          {<QRScanner />}
-        </div>
-        <div
-          className="tab-pane"
-          id="map"
-          role="tabpanel"
-          aria-labelledby="map-tab"
-        >
-          {<Map />}
+    <>
+      <div id="app" className="p-5">
+        <div id="content" className="row g-0">
+          <div className="tab-content">
+            <div
+              className="tab-pane"
+              id="home"
+              role="tabpanel"
+              aria-labelledby="home-tab"
+            >
+              {<Home />}
+            </div>
+            <div
+              className="tab-pane"
+              id="scanner"
+              role="tabpanel"
+              aria-labelledby="scanner-tab"
+            >
+              {<QRScanner />}
+            </div>
+            <div
+              className="tab-pane"
+              id="map"
+              role="tabpanel"
+              aria-labelledby="map-tab"
+            >
+              {<Map />}
+            </div>
+          </div>
         </div>
       </div>
 
-      <ul
-        className="nav nav-tabs fixed-bottom justify-content-evenly bg-light border-2 border-top border-secondary-subtle"
-        id="myTab"
-        role="tablist"
-      >
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link fs-1 fw-bold active"
-            id="home-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#home"
-            type="button"
-            role="tab"
-            aria-controls="home"
-          >
-            <i className="bi bi-house-door p-2"></i>
-            Inicio
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link fs-1 fw-bold"
-            id="scanner-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#scanner"
-            type="button"
-            role="tab"
-            aria-controls="scanner"
-          >
-            <i className="bi bi-camera p-2"></i>
-            QR
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link fs-1 fw-bold"
-            id="map-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#map"
-            type="button"
-            role="tab"
-            aria-controls="map"
-          >
-            <i className="bi bi-geo-alt p-2"></i>
-            Ubicación
-          </button>
-        </li>
-      </ul>
-    </div>
+      <div id="tabBar" className="row fixed-bottom">
+        <ol
+          className="nav nav-tabs nav-underline justify-content-evenly bg-light border-2 border-top border-dark-subtle"
+          id="tabButtons"
+          role="tablist"
+        >
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link fs-1 fw-bold d-flex align-items-center px-2"
+              id="scanner-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#scanner"
+              type="button"
+              role="tab"
+              aria-controls="scanner"
+              aria-current="page"
+            >
+              <i
+                className="bi bi-camera"
+                style={{ fontSize: 80, paddingRight: 10 }}
+              ></i>
+              QR
+            </button>
+          </li>
+
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link fs-1 fw-bold d-flex  align-items-center active px-2"
+              id="home-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#home"
+              type="button"
+              role="tab"
+              aria-controls="home"
+            >
+              <i
+                className="bi bi-house-door"
+                style={{ fontSize: 80, paddingRight: 10 }}
+              ></i>
+              Inicio
+            </button>
+          </li>
+
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link fs-1 fw-bold d-flex  align-items-center px-2"
+              id="map-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#map"
+              type="button"
+              role="tab"
+              aria-controls="map"
+            >
+              <i
+                className="bi bi-geo-alt"
+                style={{ fontSize: 80, paddingRight: 10 }}
+              ></i>
+              Ubicación
+            </button>
+          </li>
+        </ol>
+      </div>
+    </>
   );
 };
 
--- src/ui/screens/Home.tsx
+++ src/ui/screens/Home.tsx
@@ -1,8 +1,159 @@
-//import React from "react";
-//import { Link } from "react-router-dom";
-
 const Home = () => {
-  return <div>This is home page</div>;
+  return (
+    <div id="home" className="container">
+      {/* Modal */}
+      <div className="modal fade" id="modal" aria-labelledby="modalTitle">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-1 fw-bold" id="modalTitle">
+                Información:
+              </h1>
+            </div>
+            <div className="modal-body fs-2">
+              Bienvenido a tu App La Guaira, aquí encontrarás distintos
+              servicios a los que puedes acceder desde los botones disponibles
+              en esta pantalla, en la barra de navegación abajo podrás acceder a
+              las secciones QR y Ubicación. Para salir al menú, presiona el
+              botón "Cerrar".
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary fs-1 fw-bold"
+                data-bs-dismiss="modal"
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* header con mensaje e icono de info para el modal*/}
+      <div id="header" className="row justify-content-between">
+        <div className="col-10 fs-1 fw-bold align-self-center">
+          Bienvenido, Usuario!
+        </div>
+        <div className="col-2">
+          <i
+            className="bi bi-info-circle m-5 button"
+            style={{ fontSize: 80 }}
+            data-bs-toggle="modal"
+            data-bs-target="#modal"
+          ></i>
+        </div>
+      </div>
+
+      {/* menu de botones */}
+      <div id="content" className="row mt-5 justify-content-center">
+        <div id="emergencias" className="row justify-content-center mb-4">
+          <div className="col-2">
+            <i className="bi bi-hospital m-5" style={{ fontSize: 80 }}></i>
+          </div>
+          <div className="col-4 align-self-center">
+            <a
+              id="b1"
+              className="btn btn-lg btn-primary border-2 border-dark fs-1 fw-bold"
+              href="#"
+              role="button"
+            >
+              Emergencias
+            </a>
+          </div>
+        </div>
+
+        <div id="parquimetro" className="row justify-content-center mb-4">
+          <div className="col-2">
+            <i className="bi bi-car-front m-5" style={{ fontSize: 80 }}></i>
+          </div>
+          <div className="col-4 align-self-center">
+            <a
+              id="b2"
+              className="btn btn-lg btn-primary border-2 border-dark fs-1 fw-bold"
+              href="#"
+              role="button"
+            >
+              Parquímetro
+            </a>
+          </div>
+        </div>
+
+        <div id="servicios" className="row justify-content-center mb-4">
+          <div className="col-2">
+            <i
+              className="bi bi-lightbulb-fill m-5"
+              style={{ fontSize: 80 }}
+            ></i>
+          </div>
+          <div className="col-4 align-self-center">
+            <a
+              id="b3"
+              className="btn btn-lg btn-primary border-2 border-dark fs-1 fw-bold"
+              href="#"
+              role="button"
+            >
+              Servicios
+            </a>
+          </div>
+        </div>
+
+        <div id="comercio" className="row justify-content-center mb-4">
+          <div className="col-2">
+            <i className="bi bi-shop m-5" style={{ fontSize: 80 }}></i>
+          </div>
+          <div className="col-4 align-self-center">
+            <a
+              id="b4"
+              className="btn btn-lg btn-primary border-2 border-dark fs-1 fw-bold"
+              href="#"
+              role="button"
+            >
+              Comercio
+            </a>
+          </div>
+        </div>
+
+        <div id="denuncias" className="row justify-content-center mb-4">
+          <div className="col-2">
+            <i
+              className="bi bi-person-exclamation m-5"
+              style={{ fontSize: 80 }}
+            ></i>
+          </div>
+          <div className="col-4 align-self-center">
+            <a
+              id="b5"
+              className="btn btn-lg btn-primary border-2 border-dark fs-1 fw-bold"
+              href="#"
+              role="button"
+            >
+              Denuncias
+            </a>
+          </div>
+        </div>
+
+        <div id="multas" className="row justify-content-center mb-4">
+          <div className="col-2">
+            <i
+              className="bi bi-sign-no-parking m-5"
+              style={{ fontSize: 80 }}
+            ></i>
+          </div>
+          <div className="col-4 align-self-center">
+            <a
+              id="b6"
+              className="btn btn-lg btn-primary border-2 border-dark fs-1 fw-bold"
+              href="#"
+              role="button"
+            >
+              Multas
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
