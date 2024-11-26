c05d2cedc31bce84e3914e4ce96afad1b14a3f3a
Adrian-898 <adriantorres898@gmail.com>
2024-11-26 18:31:06 -0300

    created Map View

--- src/ui/screens/Map.tsx
+++ src/ui/screens/Map.tsx
@@ -2,10 +2,10 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const Map = () => {
-  const position = { lat: 51.505, lng: -0.09, alt: 1 };
+  const position = { lat: 10.597032, lng: -66.930431 };
 
   return (
-    <div id="map" className="container-fluid">
+    <div id="map" className="fixed-top">
       <MapContainer
         center={position}
         zoom={13}
@@ -15,10 +15,8 @@ const Map = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={position} autoPan>
+          <Popup>Marcador</Popup>
         </Marker>
       </MapContainer>
     </div>
