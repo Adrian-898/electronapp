8ab5537e37a30f96558c9504df4db02e9c3e8449
Adrian-898 <adriantorres898@gmail.com>
2024-11-27 18:27:42 -0300

    bugfixes & tweaks

--- src/ui/screens/Map.tsx
+++ src/ui/screens/Map.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import type { Map } from "leaflet";
-import "leaflet/dist/leaflet.css";
 
 // objeto para almacenar informacion de los lugares que se quiere marcar en el mapa
 type Lugar = {
