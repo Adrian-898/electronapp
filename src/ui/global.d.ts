8353d94efbb5c6a6681e15e72bce968bb0d1e0a2
Adrian-898 <adriantorres898@gmail.com>
2024-12-19 15:48:39 -0400

    Added DataTables to Multas

---
+++ src/ui/global.d.ts
@@ -0,0 +1,4 @@
+declare module "datatables.net-plugins/i18n/es-ES.mjs" {
+  const content: any;
+  export default content;
+}
--- src/ui/screens/Multas.tsx
+++ src/ui/screens/Multas.tsx
@@ -1,12 +1,56 @@
+import "jquery";
+import DataTable from "datatables.net-react";
+import DT from "datatables.net-bs5";
+import PdfPrinter from "pdfmake";
+import language from "datatables.net-plugins/i18n/es-ES.mjs";
+// import DateTime from "datatables.net-datetime";
+import "datatables.net-buttons-bs5";
+import "datatables.net-buttons/js/buttons.colVis.mjs";
+import "datatables.net-buttons/js/buttons.html5.mjs";
+import "datatables.net-searchbuilder-bs5";
+import "datatables.net-searchpanes-bs5";
 import "./styles.css";
 
+DataTable.use(DT);
+DT.Buttons.pdfMake(PdfPrinter);
+
 const Multas = () => {
+  const cols = [
+    { data: "name" },
+    { data: "position" },
+    { data: "office" },
+    { data: "extn" },
+    { data: "start_date" },
+    { data: "salary" },
+  ];
+
   return (
-    <div
-      id="multas"
-      className="container-fluid bottom-margin bg-primary-subtle"
-    >
-      Hola
+    <div id="multas" className="container-fluid bottom-margin">
+      <h1>Tabla</h1>
+      <h2>DataTables + React</h2>
+      <DataTable
+        className="container w-100"
+        ajax="/src/tests/data.json"
+        columns={cols}
+        onDraw={() => console.log("Tabla dibujada")}
+        options={{
+          language,
+          layout: {
+            topStart: "buttons",
+          },
+        }}
+      >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Extn.</th>
+            <th>Start date</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+      </DataTable>
     </div>
   );
 };
