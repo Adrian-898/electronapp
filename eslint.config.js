0fe2fed31652e854f24e090f385784a790f96385
Adrian-898 <adriantorres898@gmail.com>
2025-01-15 17:05:21 -0400

    tweaks

--- eslint.config.js
+++ eslint.config.js
@@ -16,10 +16,10 @@ export default tseslint.config({
 	files: ['**/*.{ts,tsx}'],
 
 	languageOptions: {
-		ecmaVersion: 'latest',
+		ecmaVersion: 2020,
 		sourceType: 'module',
 		globals: {
-			...globals.browser,
+			...globals.node,
 		},
 		parserOptions: {
 			emitDecoratorMetadata: true,
