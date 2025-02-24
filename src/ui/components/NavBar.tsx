c8684cd1fecf35e2882b80645f1fccd6e5fff5c0
Adrian-898 <adriantorres898@gmail.com>
2025-02-24 15:04:45 -0400

    simplified navigation using NavLink from react router

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -1,60 +1,42 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-	const [activeTab, setActiveTab] = useState('home-tab');
-
 	return (
 		<nav id='tabBar' className='row fixed-bottom'>
-			<ol
+			<ul
 				className='nav nav-tabs nav-underline justify-content-evenly bg-light border-2 border-top border-dark-subtle'
 				id='tabButtons'
 			>
-				<li className='nav-item'>
-					<Link
-						className={`nav-link fs-1 fw-bold d-flex align-items-center px-4 ${
-							activeTab === 'scanner-tab' ? ' active' : ''
-						}`}
-						id='scanner-tab'
-						type='button'
-						to={'/QRScanner'}
-						onClick={() => setActiveTab('scanner-tab')}
-					>
-						<i className='bi bi-camera tab-bar-icon'></i>
-						Escáner QR
-					</Link>
-				</li>
+				<NavLink
+					className={'nav-link fs-1 fw-bold d-flex align-items-center px-4'}
+					id='scanner-tab'
+					type='button'
+					to={'/QRScanner'}
+				>
+					<i className='bi bi-camera tab-bar-icon' />
+					Escáner QR
+				</NavLink>
 
-				<li className='nav-item'>
-					<Link
-						className={`nav-link fs-1 fw-bold d-flex align-items-center px-4 ${
-							activeTab === 'home-tab' ? ' active' : ''
-						}`}
-						id='home-tab'
-						type='button'
-						to={'/'}
-						onClick={() => setActiveTab('home-tab')}
-					>
-						<i className='bi bi-house-door tab-bar-icon'></i>
-						Inicio
-					</Link>
-				</li>
+				<NavLink
+					className={'nav-link fs-1 fw-bold d-flex align-items-center px-4'}
+					id='home-tab'
+					type='button'
+					to={'/'}
+				>
+					<i className='bi bi-house-door tab-bar-icon'></i>
+					Inicio
+				</NavLink>
 
-				<li className='nav-item'>
-					<Link
-						className={`nav-link fs-1 fw-bold d-flex align-items-center px-4 ${
-							activeTab === 'map-tab' ? ' active' : ''
-						}`}
-						id='map-tab'
-						type='button'
-						to={'/Map'}
-						onClick={() => setActiveTab('map-tab')}
-					>
-						<i className='bi bi-geo-alt tab-bar-icon'></i>
-						Ubicación
-					</Link>
-				</li>
-			</ol>
+				<NavLink
+					className={'nav-link fs-1 fw-bold d-flex align-items-center px-4'}
+					id='map-tab'
+					type='button'
+					to={'/Map'}
+				>
+					<i className='bi bi-geo-alt tab-bar-icon'></i>
+					Ubicación
+				</NavLink>
+			</ul>
 		</nav>
 	);
 };
